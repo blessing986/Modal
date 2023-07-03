125c70fd796ed5a247fdb7a5a5773bc0dbb5ab6c
Blessing U <blessing986@users.noreply.github.com>
2023-07-03 22:45:07 +0100

    built modal

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosedModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closedModel = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModel);
+}
+
+btnClosedModal.addEventListener('click', closedModel);
+overlay.addEventListener('click', closedModel);
+
+document.addEventListener('keydown', function (e) {
+//   console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closedModel();
+  }
+});
